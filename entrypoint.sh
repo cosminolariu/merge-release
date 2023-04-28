7d4386ce927b5397c3b8f98103e41739f4f60621
Cosmin OLARIU <cosmin.olariu@decisionfocus.com>
2023-04-28 10:40:03 +0300

    ac8

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,7 @@ fi
 
 # initialize git
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+echo "${remote_repo}"
 git config http.sslVerify false
 git config user.name "Merge Release"
 git config user.email "actions@users.noreply.github.com"
@@ -35,6 +36,8 @@ git remote add merge-release "${remote_repo}"
 git remote --verbose
 git show-ref # useful for debugging
 git branch --verbose
+
+ls
 
 # Dependencies are installed at build time
 node /src/merge-release-run.js "$@" || exit 1
